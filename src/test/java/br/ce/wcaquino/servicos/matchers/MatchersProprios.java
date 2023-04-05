c651109ef539ba054b6e1c0fc370842015ecf44b
Edson Santos <cmo.edson@gmail.com>
2023-04-04 22:43:14 -0300

    Implementando Desafio

--- src/test/java/br/ce/wcaquino/servicos/matchers/MatchersProprios.java
+++ src/test/java/br/ce/wcaquino/servicos/matchers/MatchersProprios.java
@@ -8,9 +8,16 @@ public class MatchersProprios {
 		return new DiaSemanaMatcher(diaSemana);
 	}
 	
-	
 	public static DiaSemanaMatcher caiNumaSegunda() {
 		return new DiaSemanaMatcher(Calendar.MONDAY);
+	}
+	
+	public static DataDiferencaDiasMatcher ehHojeComDiferencaDias(Integer qtdDias) {
+		return new DataDiferencaDiasMatcher(qtdDias);
+	}
+	
+	public static DataDiferencaDiasMatcher ehHoje() {
+		return new DataDiferencaDiasMatcher(0);
 	}
 
 }
