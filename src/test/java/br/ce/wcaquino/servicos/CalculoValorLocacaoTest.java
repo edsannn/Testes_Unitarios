0e11a15c187a8f8fa54dc76ca8f36eb36decec91
Edson Santos <cmo.edson@gmail.com>
2023-04-04 23:31:46 -0300

    Criação de dados para os testes

--- src/test/java/br/ce/wcaquino/servicos/CalculoValorLocacaoTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculoValorLocacaoTest.java
@@ -1,5 +1,7 @@
 package br.ce.wcaquino.servicos;
 
+import static br.ce.wcaquino.builders.FilmeBuilder.umFilme;
+import static br.ce.wcaquino.builders.UsuarioBuilder.umUsuario;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -27,41 +29,39 @@ public class CalculoValorLocacaoTest {
 
 	@Parameter
 	public List<Filme> filmes;
-	@Parameter(value=1)
+	@Parameter(value = 1)
 	public Double valorLocacao;
 
-	@Parameter(value=2)
+	@Parameter(value = 2)
 	public String cenario;
-	
+
 	@Before
 	public void setup() {
 		service = new LocacaoService();
 	}
 
-	private static Filme filme1 = new Filme("Filme 1", 2, 4.0);
-	private static Filme filme2 = new Filme("Filme 2", 2, 4.0);
-	private static Filme filme3 = new Filme("Filme 3", 2, 4.0);
-	private static Filme filme4 = new Filme("Filme 4", 2, 4.0);
-	private static Filme filme5 = new Filme("Filme 5", 2, 4.0);
-	private static Filme filme6 = new Filme("Filme 6", 2, 4.0);
-	private static Filme filme7 = new Filme("Filme 7", 2, 4.0);
-
+	private static Filme filme1 = umFilme().agora();
+	private static Filme filme2 = umFilme().agora();
+	private static Filme filme3 = umFilme().agora();
+	private static Filme filme4 = umFilme().agora();
+	private static Filme filme5 = umFilme().agora();
+	private static Filme filme6 = umFilme().agora();
+	private static Filme filme7 = umFilme().agora();
 
 	@Parameters(name = "{2}")
 	public static Collection<Object[]> getParametros() {
-		return Arrays.asList(new Object[][] { 
-			{Arrays.asList(filme1, filme2), 8.0,"Sem desconto" },
-			{Arrays.asList(filme1, filme2, filme3), 11.0,"3 filmes; 25%" },
-			{Arrays.asList(filme1, filme2, filme3, filme4), 13.0,"4 filmes; 50%" },
-			{Arrays.asList(filme1, filme2, filme3, filme4, filme5), 14.0, "5 filmes; 75%" },
-			{Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 14.0, "6 filmes; 100%" }, 
-			{Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6, filme7), 18.0, "7 filmes; 100%" } });
+		return Arrays.asList(new Object[][] { { Arrays.asList(filme1, filme2), 8.0, "Sem desconto" },
+				{ Arrays.asList(filme1, filme2, filme3), 11.0, "3 filmes; 25%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4), 13.0, "4 filmes; 50%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5), 14.0, "5 filmes; 75%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 14.0, "6 filmes; 100%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6, filme7), 18.0, "7 filmes; 100%" } });
 	}
 
 	@Test
 	public void deveCalcularValorLocacaoConsiderandoDescontos() throws FilmeSemEstoqueException, LocadoraException {
 		// cenario
-		Usuario usuario = new Usuario("Usuario 1");
+		Usuario usuario = umUsuario().agora();
 
 		// acao
 		Locacao resultado = service.alugarFilme(usuario, filmes);
